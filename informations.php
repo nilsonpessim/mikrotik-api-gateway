aa8ee15f7c356381c96ab889732b58af249c81ca
Nilson Pessim <nilson@techlabs.net.br>
2023-09-19 14:35:58 -0300

    Update informations.php

--- informations.php
+++ informations.php
@@ -1,1 +1,21 @@
-oi
+<div class="col-sm-4 mb-3">
+    <div class="card card-body">
+        <h4 class="card-title mb-1">Último Release:</h4>
+        <p class="card-text">v.0.1.1 - Data</p>
+        <a href="https://github.com/nilsonpessim/mikrotik-api-gateway/releases" target="_blank" class="btn btn-danger">Ver Todos</a>
+    </div>
+</div>
+
+<div class="col-sm-4 mb-3">
+    <div class="card card-body">
+        <h4 class="card-title mb-1">Documentação:</h4>
+        <p class="card-text">Acesse nossa documentação completa.</p>
+        <a href="https://github.com/nilsonpessim/mikrotik-api-gateway/wiki" target="_blank" class="btn btn-danger">Acessar</a>
+    </div>
+</div>
+
+<div class="col-sm-12 mb-3">
+    <div class="card card-body">
+        <iframe width="100%" height="600" src="https://www.youtube.com/embed/9BnESNq4xAk?si=hADmpdvjct0cz_39&amp;controls=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
